6fa3a0de5ac536333563e510363a5398d524fa42
benjenkinsv95 <benjenkinsv95@gmail.com>
2021-12-21 15:05:55 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import paragraphs from './fortuna-paragraphs'
+import useWindowDimensions from './useWindowDimensions';
+import React, { useState } from 'react'
+import ReactPlayer from 'react-player'
+
+
+const lyricsStyles = {
+  minWidth: '400px',
+  fontSize: '1.5rem',
+  padding: '1rem',
+  overflowY: 'auto',
+  position: 'relative',
+  color: 'white',
+  background: 'black'
+}
+
+const videoStyles = {
+  width: '100%',
+  minHeight: '400px',
+  background: 'black'
+}
+
+function Lyrics ({ player, setPlaying }) {
+  const handleClick = sentence => {
+    const timestampSeconds =
+  sentence.minutesTimestamp * 60 + sentence.secondsTimestamp
+
+    console.log(player, timestampSeconds)
+    player?.seekTo(timestampSeconds)
+  }
+  
+  const paragraphElements = paragraphs.map((paragraph, index) => {
+    const sentenceElements = paragraph.sentences.map((sentence, j) => <p id={sentence.id} key={j} onClick={() => handleClick(sentence)}><small>[{sentence.minutesTimestamp}:{sentence.secondsTimestamp.toString().padStart(2, '0')}]</small> {sentence.latinSentence} <br/><small style={{ color: 'cyan', fontWeight: '500' }}>{sentence.englishSentence}</small></p>)
+    return <div key={index} style={{ borderBottom: '2px solid white'}}>{sentenceElements}</div>
+  })
+
+  return (
+    <div style = { lyricsStyles }>
+      {paragraphElements}
+    </div>
+  )
+}
+
+function Video ({ setPlayer, playing, setPlaying }) {
+  
+  const handleProgress = event => {
+    const playedSeconds = Math.floor(event.playedSeconds)
+    const sentences = []
+    paragraphs.forEach(p => sentences.push(...p.sentences))
+
+    const currentSentence = sentences.find(sentence => {
+      const timestampSeconds = (sentence.minutesTimestamp * 60) + sentence.secondsTimestamp
+      return timestampSeconds === playedSeconds
+    })
+
+    if (currentSentence) {
+      console.log(currentSentence)
+      document.getElementById(currentSentence
+.id).scrollIntoView()
+    }
+  }
+  return (
+    <ReactPlayer width='100%' controls={true} height='100%' playing={playing} onPlay={isPlaying => setPlaying(isPlaying)} url='https://www.youtube.com/watch?v=00YI0OlQQ6s' onProgress={handleProgress}
+    ref={player => setPlayer(player)} />
+  )
+}
 
 function App() {
+  const { width } = useWindowDimensions()
+
+  const containerStyles = {
+  display: 'flex',
+  width: '100%',
+  height: '100vh',
+  flexDirection: width > 900 ? 'row' : 'column'
+}
+
+  const [player, setPlayer] = useState(null)
+  const [playing, setPlaying] = useState(false)
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyles}>
+      <Video setPlayer={setPlayer} playing={playing} setPlaying={setPlaying} />
+      <Lyrics player={player} setPlaying = { setPlaying }
+ />
     </div>
   );
 }
---
+++ src/fortuna-paragraphs.js
@@ -0,0 +1,239 @@
+const paragraphs = [
+  {
+    "sentences": [
+      {
+        "englishSentence": "O Fortune",
+        "latinSentence": "O Fortuna",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 5
+      },
+      {
+        "englishSentence": "Like the moon",
+        "latinSentence": "Velut luna",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 11
+      },
+      {
+        "englishSentence": "You are changeable",
+        "latinSentence": "Statu variabilis",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 17
+      },
+      {
+        "englishSentence": "Ever waxing and waning",
+        "latinSentence": "Semper crescis aut decrescis",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 27
+      },
+      {
+        "englishSentence": "Hateful life",
+        "latinSentence": "Vita detestabilis",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 34
+      },
+      {
+        "englishSentence": "First oppresses",
+        "latinSentence": "Nunc obdurat",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 40
+      },
+      {
+        "englishSentence": "And then soothes",
+        "latinSentence": "Et tunc curat",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 42
+      },
+      {
+        "englishSentence": "As fancy takes it",
+        "latinSentence": "Ludo mentis aciem,",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 46
+      },
+      {
+        "englishSentence": "Poverty",
+        "latinSentence": "Egestatem,",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 51
+      },
+      {
+        "englishSentence": "Power",
+        "latinSentence": "Potestatem",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 54
+      },
+      {
+        "englishSentence": "It melts them like ice",
+        "latinSentence": "Dissolvit ut glaciem.",
+        "minutesTimestamp": 0,
+        "secondsTimestamp": 57
+      }
+    ]
+  },
+  {
+    "sentences": [
+      {
+        "englishSentence": "Fate - monstrous",
+        "latinSentence": "Sors immanis",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 3
+      },
+      {
+        "englishSentence": "and empty",
+        "latinSentence": "Et inanis,",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 6
+      },
+      {
+        "englishSentence": "You whirling wheel",
+        "latinSentence": "Rota tu volubilis",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 10
+      },
+      {
+        "englishSentence": "You are malevolent",
+        "latinSentence": "Status malus",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 15
+      },
+      {
+        "englishSentence": "Well-being is vain",
+        "latinSentence": "Vana salus",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 17
+      },
+      {
+        "englishSentence": "And always fades to nothing",
+        "latinSentence": "Semper dissolubilis",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 21
+      }
+    ]
+  },
+  {
+    "sentences": [
+      {
+        "englishSentence": "Shadowed",
+        "latinSentence": "Obumbrata",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 27
+      },
+      {
+        "englishSentence": "And vailed",
+        "latinSentence": "Et velata",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 30
+      },
+      {
+        "englishSentence": "You plague me too",
+        "latinSentence": "Michi quoque niteris",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 32
+      },
+      {
+        "englishSentence": "Now through the game",
+        "latinSentence": "Nunc per ludum",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 38
+      },
+      {
+        "englishSentence": "My bare back",
+        "latinSentence": "Dorsum nudum",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 41
+      },
+      {
+        "englishSentence": "I bring to your villainy",
+        "latinSentence": "Fero tui sceleris.",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 44
+      }
+    ]
+  },
+  {
+    "sentences": [
+      {
+        "englishSentence": "Fate - in health",
+        "latinSentence": "Sors salutis",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 50
+      },
+      {
+        "englishSentence": "And virtue",
+        "latinSentence": "Et virtutis",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 53
+      },
+      {
+        "englishSentence": "is now against me",
+        "latinSentence": "Michi nunc contraria,",
+        "minutesTimestamp": 1,
+        "secondsTimestamp": 56
+      },
+      {
+        "englishSentence": "Driven on",
+        "latinSentence": "Est affectus",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 1
+      },
+      {
+        "englishSentence": "And weighted down",
+        "latinSentence": "Et defectus",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 4
+      },
+      {
+        "englishSentence": "always enslaved",
+        "latinSentence": "Semper in angaria.",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 7
+      },
+      {
+        "englishSentence": "(So) at this hour",
+        "latinSentence": "Hac in hora",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 13
+      },
+      {
+        "englishSentence": "without delay",
+        "latinSentence": "Sine mora",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 15
+      },
+      {
+        "englishSentence": "Pluck the vibrating strings",
+        "latinSentence": "Corde pulsum tangite",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 18
+      },
+      {
+        "englishSentence": "Since (through) fate",
+        "latinSentence": "Quod per sortem",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 23
+      },
+      {
+        "englishSentence": "Strikes the strong man",
+        "latinSentence": "Sternit fortem",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 27
+      },
+      {
+        "englishSentence": "Everyone weep with me",
+        "latinSentence": "Mecum omnes plangite",
+        "minutesTimestamp": 2,
+        "secondsTimestamp": 29
+      }
+    ]
+  }
+]
+
+const sanitized = paragraphs.map((p, pIndex) => ({
+    ...p,
+    id: `p${pIndex}`,
+    sentences: p.sentences.map((s, sIndex) => ({
+        ...s,
+        id: `p${pIndex} - ${sIndex}`
+    }))
+}))
+
+export default sanitized
